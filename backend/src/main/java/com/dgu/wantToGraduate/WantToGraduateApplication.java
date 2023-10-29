a9f733e0cf9b16b093d4db322b06325187ed3624
Limwngur <ovg07047@naver.com>
2023-10-29 14:25:15 +0900

    init: Server 초기설정

---
+++ backend/src/main/java/com/dgu/wantToGraduate/WantToGraduateApplication.java
@@ -0,0 +1,13 @@
+package com.dgu.wantToGraduate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WantToGraduateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WantToGraduateApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/dgu/wantToGraduate/WantToGraduateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dgu.wantToGraduate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WantToGraduateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
