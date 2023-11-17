848a73cf2b04be4e4bb08f66a72ff61aa20cc8e0
JuhyunPark1831 <epahs1831@naver.com>
2023-11-17 18:26:17 +0900

    Feat: 사용자 정보 조회 기능 구현

---
+++ backend/src/main/java/com/dgu/wantToGraduate/domain/user/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.dgu.wantToGraduate.domain.user.repository;
+
+import com.dgu.wantToGraduate.domain.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
