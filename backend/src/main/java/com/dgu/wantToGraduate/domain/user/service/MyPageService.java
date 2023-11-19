35ce1b2675dfa58c5051f4bd60faa4918d228f0f
JuhyunPark1831 <epahs1831@naver.com>
2023-11-19 23:33:35 +0900

    Refactor: 사용자 정보 조회 기능 수정 및 리뷰 엔티티 수정

--- backend/src/main/java/com/dgu/wantToGraduate/domain/user/service/MyPageService.java
+++ backend/src/main/java/com/dgu/wantToGraduate/domain/user/service/MyPageService.java
@@ -15,16 +15,12 @@ public class MyPageService {
     private final UserRepository userRepository;
 
     public MyPageDto.MyPageResponseDto getMyPage(Long userId) {
-        User user = userRepository.findById(userId).get();
-        if (user != null) {
-            return MyPageDto.MyPageResponseDto.builder()
-                    .nickname(user.getNickname())
-                    .userid(user.getId())
-                    .grade(user.getGrade())
-                    .build();
-        } else {
-            // 유저를 찾지 못한 경우 처리. 예를 들어 예외를 던지거나 기본 DTO를 반환하는 등의 방식으로 처리 가능
-            return null; // 또는 throw new UserNotFoundException("ID에 해당하는 사용자를 찾을 수 없습니다: " + userId);
-        }
+        return userRepository.findById(userId)
+                .map(user -> MyPageDto.MyPageResponseDto.builder()
+                        .nickname(user.getNickname())
+                        .userid(user.getId())
+                        .grade(user.getGrade())
+                        .build())
+                .orElseThrow(() -> new IllegalArgumentException("해당 사용자 정보가 없습니다: " + userId));
     }
 }
