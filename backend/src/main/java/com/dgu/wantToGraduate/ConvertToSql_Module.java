7be571d6ea0f755432a39cdb40398c3da2108a80
Limwngur <ovg07047@naver.com>
2023-11-24 15:47:45 +0900

    Refactor: 변환기 포맷 수정

--- backend/src/main/java/com/dgu/wantToGraduate/ConvertToSql_Module.java
+++ backend/src/main/java/com/dgu/wantToGraduate/ConvertToSql_Module.java
@@ -22,8 +22,9 @@ public class ConvertToSql_Module {
                 if (parts.length > 1) {
                     categorySet(parts[1]);// 카테고리 분류 집합(확인용)
 
+
                     String storeName = parts[0].trim();
-                    String sql = "INSERT INTO brand (brand_name, brand_category) VALUES ('" + storeName + "', '" + parts[1] + "');";
+                    String sql = "CALL InsertBrand('" + storeName + "', '" + enumNameof(parts[1]) + "');";
                     insertQueries.add(sql);
                 }
             }
@@ -42,5 +43,22 @@ public class ConvertToSql_Module {
     private static HashSet<String> categorySet(String data) {
         set.add(data);
         return set;
+    }
+
+    static String enumNameof(String categoryName){
+        switch(categoryName){
+            case "커피점/카페": return "CAFE";
+            case "양식": return "WESTERN";
+            case "중식": return "CHINESE";
+            case "제과제빵떡케익": return "BAKERY";
+            case "닭/오리요리": return "CHICKEN";
+            case "일식/수산물": return "JAPANESE";
+            case "한식": return "KOREAN";
+            case "별식/퓨전요리": return "FUSION";
+            case "패스트푸드": return "FASTFOOD";
+            case "분식": return "SNACK";
+            case "유흥주점": return "PUB";
+            default: return new Exception("잘못된 카테고리명입니다.").getMessage();
+        }
     }
 }
